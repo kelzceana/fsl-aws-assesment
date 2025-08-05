cddf9dc945cef07c633d97915abe391da5d70ccc
Kelechi Obikaonu <71353368+kelzceana@users.noreply.github.com>
2025-08-05 02:12:38 -0400

    cicd  pipeline

--- codebase/rdicidr-0.1.0/terraform/module/app/main.tf
+++ codebase/rdicidr-0.1.0/terraform/module/app/main.tf
@@ -38,6 +38,30 @@ resource "aws_s3_bucket_public_access_block" "cloudfront_logs_pab" {
   depends_on = [aws_s3_bucket_ownership_controls.cloudfront_logs_ownership]
 }
 
+resource "aws_s3_bucket_policy" "fsl_bucket_policy" {
+  bucket = aws_s3_bucket.fsl_bucket.id
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontAccess"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.fsl_bucket.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket_acl" "cloudfront_logs_acl" {
   depends_on = [
     aws_s3_bucket_ownership_controls.cloudfront_logs_ownership,
